b507c8a6ade38a616453b3167053340cc5f68952
manuelamercado <mmercado@intellisys.com.do>
2020-06-20 21:15:27 -0400

    Creating menu

---
+++ app/nuxt.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'Dynamic Menu',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      { src: 'https://kit.fontawesome.com/0b56a41f03.js', crossorigin: "anonymous"}
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  /*
+  ** Environment variables
+  */
+  env: {
+    airtableApiKey: process.env.AIRTABLE_API_KEY || '',
+    airtableDBId: process.env.AIRTABLE_DB_ID || ''
+  }
+}
